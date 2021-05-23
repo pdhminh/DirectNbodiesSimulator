e1741cd7085b87ea900f6738e7b2354e5755266e
Hoang Minh Pham Dinh <hoangminhphamdinhh@Hoangs-MacBook-Pro.local>
2021-05-23 09:55:37 +0700

    shorter and prettier code

--- Nbodies.py
+++ Nbodies.py
@@ -2,18 +2,19 @@ import random as rd
 import math
 import numpy as np
 from vpython import *
+
 scene2 = canvas(title='N bodies simulator', x=0, y=0, width=800, height=800, center=vector(0, 0, 0)) # background=vector(1, 1, 1
 G = 6.67408e-11
 collided = False
 planets = []
-# a = float(input('softing const: '))
 
 
 class Planets(object):
     def __init__(self, xo, yo, zo, vox, voy, voz, mass, radius):
         self.velocity = [vox, voy, voz]
         self.mass = mass
-        self.shape = simple_sphere(pos=vector(xo, yo, zo), radius=radius, color=vector(0, 1, 0))
+        self.shape = simple_sphere(pos=vector(xo, yo ,zo), radius=radius, color=vector(0, 1, 0))
+        self.pos = [self.shape.pos.x, self.shape.pos.y, self.shape.pos.z]
 
 
 def create_planet(x, y, z, vx, vy, vz, m, d):
@@ -37,24 +38,42 @@ def mergingStars(p1, p2):
         collided = False
 
 
-def calca(target, kind, x, y, z):
+def calca(target, kind, data):
     res = 0
     for planet in planets:
         if planet != target:
-            distance = math.sqrt((planet.shape.pos.x - x)**2 + (planet.shape.pos.y - y)**2 + (planet.shape.pos.z - z)**2)
+            if kind == 0:
+                distance = math.sqrt((planet.pos[0] - data[0])**2 + (planet.pos[1] - target.pos[1])**2 + (planet.pos[2] - target.pos[2])**2)
+            if kind == 1:
+                distance = math.sqrt((planet.pos[0] - target.pos[0])**2 + (planet.pos[1] - data[1])**2 + (planet.pos[2] - target.pos[2])**2)
+            if kind == 2:
+                distance = math.sqrt((planet.pos[0] - target.pos[0])**2 + (planet.pos[1] - target.pos[1])**2 + (planet.pos[2] - data[2])**2)
+            
             if distance < planet.shape.radius + target.shape.radius:
                 collided = True
                 mergingStars(target, planet)
                 break
-                # return 0
             F = G * planet.mass * target.mass / (distance**2)
-            if kind == 0:
-                res = res + F * (planet.shape.pos.x - x) / distance
-            elif kind == 1:
-                res = res + F * (planet.shape.pos.y - y) / distance
-            elif kind == 2:
-                res = res + F * (planet.shape.pos.z - z) / distance
+            res = res + F * (planet.pos[kind] - data[kind]) / distance
     return res/target.mass
+
+
+def ODE(planet, func, index, dt):
+    # RK4th method
+    data = [0, 0, 0]
+    data[index] = planet.pos[index]     
+    k1 = func(planet, index, data)
+    data[index] = planet.pos[index] + planet.velocity[index] * dt/2 + 0.5 * k1 * (dt/2)**2
+    k2 = func(planet, index, data)
+    data[index] = planet.pos[index] + planet.velocity[index] * dt/2 + 0.5 * k2 * (dt/2)**2
+    k3 = func(planet, index, data)
+    data[index] = planet.pos[index] + planet.velocity[index] * dt + 0.5 * k3 * dt**2
+    k4 = func(planet, index, data)
+    a = (1/6)*(k1 + 2*k2 + 2*k3 + k4)
+    planet.pos[index] = planet.pos[index] + planet.velocity[index] * dt + 0.5 * a * dt ** 2
+    planet.velocity[index] = planet.velocity[index] + a * dt
+    return planet.pos[index]
+
 
 
 N = int(input("Bodies: "))
@@ -73,41 +92,9 @@ print('=======================')
 dt = float(input('time interval: '))
 time = 10000
 t = 0
+
 while not collided:
     rate(60)
     for planet in planets:
-        # RK4th method
-        k1 = calca(planet, 0, x=planet.shape.pos.x, y=planet.shape.pos.y, z=planet.shape.pos.z)
-        x2 = planet.shape.pos.x + planet.velocity[0] * dt/2 + 0.5 * k1 * (dt/2)**2
-        k2 = calca(planet, 0, x=x2, y=planet.shape.pos.y, z=planet.shape.pos.z)
-        x3 = planet.shape.pos.x + planet.velocity[0] * dt/2 + 0.5 * k2 * (dt/2)**2
-        k3 = calca(planet, 0, x=x3, y=planet.shape.pos.y, z=planet.shape.pos.z)
-        x4 = planet.shape.pos.x + planet.velocity[0] * dt + 0.5 * k3 * dt**2
-        k4 = calca(planet, 0, x=x4, y=planet.shape.pos.y, z=planet.shape.pos.z)
-        ax = (1/6)*(k1 + 2*k2 + 2*k3 + k4)
-        planet.shape.pos.x = planet.shape.pos.x + planet.velocity[0]*dt + 0.5*ax*dt**2
-        planet.velocity[0] = planet.velocity[0] + ax * dt
-
-        k1 = calca(planet, 1, x=planet.shape.pos.x, y=planet.shape.pos.y, z=planet.shape.pos.z)
-        y2 = planet.shape.pos.y + planet.velocity[1] * dt/2 + 0.5 * k1 * (dt/2)**2
-        k2 = calca(planet, 1, x=planet.shape.pos.x, y=y2, z=planet.shape.pos.z)
-        y3 = planet.shape.pos.y + planet.velocity[1] * dt/2 + 0.5 * k2 * (dt/2)**2
-        k3 = calca(planet, 1, x=planet.shape.pos.x, y=y3, z=planet.shape.pos.z)
-        y4 = planet.shape.pos.y + planet.velocity[1] * dt + 0.5 * k3 * dt**2
-        k4 = calca(planet, 1, x=planet.shape.pos.x, y=y4, z=planet.shape.pos.z)
-        ay = (1/6)*(k1 + 2*k2 + 2*k3 + k4)
-        planet.shape.pos.y = planet.shape.pos.y + planet.velocity[1]*dt + 0.5*ay*dt**2
-        planet.velocity[1] = planet.velocity[1] + ay * dt
-
-        k1 = calca(planet, 2, x=planet.shape.pos.x, y=planet.shape.pos.y, z=planet.shape.pos.z)
-        z2 = planet.shape.pos.z + planet.velocity[2] * dt / 2 + 0.5 * k1 * (dt / 2) ** 2
-        k2 = calca(planet, 2, x=planet.shape.pos.x, y=planet.shape.pos.y, z=z2)
-        z3 = planet.shape.pos.z + planet.velocity[2] * dt / 2 + 0.5 * k2 * (dt / 2) ** 2
-        k3 = calca(planet, 2, x=planet.shape.pos.x, y=planet.shape.pos.y, z=z3)
-        z4 = planet.shape.pos.z + planet.velocity[2] * dt + 0.5 * k3 * dt ** 2
-        k4 = calca(planet, 2, x=planet.shape.pos.x, y=planet.shape.pos.y, z=z4)
-        az = (1 / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
-        planet.shape.pos.z = planet.shape.pos.z + planet.velocity[2] * dt + 0.5 * az * dt ** 2
-        planet.velocity[2] = planet.velocity[2] + az * dt
-
+        planet.shape.pos = vector(ODE(planet, calca, 0, dt), ODE(planet, calca, 1, dt), ODE(planet, calca, 2, dt))
     t = t + dt
